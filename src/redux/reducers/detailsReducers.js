1c506b0b98d31f56eaaeeebed8c54e6fd2b89a2b
Kashkarov Chyngyz <chikabiku4@gmail.com>
2023-12-28 11:01:25 +0600

    chyngyz

---
+++ src/redux/reducers/detailsReducers.js
@@ -0,0 +1,16 @@
+import { SEARCH_DETAILS } from "../actions/types";
+
+const initialState = {
+    data: {}
+}
+
+const detailsReducer = (state = initialState, action) => {
+    switch(action.type) {
+        case SEARCH_DETAILS:
+            return {...state, data:action.payload}
+        default:
+            return state
+    }
+}
+
+export default detailsReducer
